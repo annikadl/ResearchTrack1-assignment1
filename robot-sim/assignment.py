427b1012445e5de86ac79502f0cf19b1821064f4
annikadl <annikadelucchi25@gmail.com>
2023-10-18 07:42:34 +0000

    primitive ex3

--- robot-sim/assignment.py
+++ robot-sim/assignment.py
@@ -3,10 +3,81 @@ from __future__ import print_function
 import time
 from sr.robot import *
 
-a = dist;
+a_th = 2.0
+""" float: Threshold for the control of the orientation"""
+
+d_th = 0.4
+""" float: Threshold for the control of the linear distance"""
 
 R = Robot()
+""" instance of the class Robot"""
 
+def drive(speed, seconds):
+    """
+    Function for setting a linear velocity
+    
+    Args: speed (int): the speed of the wheels
+	  seconds (int): the time interval
+    """
+    R.motors[0].m0.power = speed
+    R.motors[0].m1.power = speed
+    time.sleep(seconds)
+    R.motors[0].m0.power = 0
+    R.motors[0].m1.power = 0
 
-while 1:
-    pass
+def turn(speed, seconds):
+    """
+    Function for setting an angular velocity
+    
+    Args: speed (int): the speed of the wheels
+	  seconds (int): the time interval
+    """
+    R.motors[0].m0.power = speed
+    R.motors[0].m1.power = -speed
+    time.sleep(seconds)
+    R.motors[0].m0.power = 0
+    R.motors[0].m1.power = 0
+
+def find_token():
+    """
+    Function to find the closest token
+
+    Returns:
+	dist (float): distance of the closest token (-1 if no token is detected)
+	rot_y (float): angle between the robot and the token (-1 if no token is detected)
+    """
+    dist=100
+    for token in R.see():
+	    if dist==100:
+		return -1, -1
+	    else:
+	   	return dist, rot_y
+	   	
+def grab_token():
+	while 1:
+	    dist, rot_y = find_token()  # we look for markers
+	    if dist==-1:
+		print("I don't see any token!!")
+		exit()  # if no markers are detected, the program ends
+	    elif dist <d_th: 
+		print("Found it!")
+		R.grab() # if we are close to the token, we grab it.
+		print("Gotcha!") 
+		exit()
+	    elif -a_th<= rot_y <= a_th: # if the robot is well aligned with the token, we go forward
+		print("Ah, here we are!.")
+		drive(10, 0.5)
+	    elif rot_y < -a_th: # if the robot is not well aligned with the token, we move it on the left or on the right
+		print("Left a bit...")
+		turn(-2, 0.5)
+	    elif rot_y > a_th:
+		print("Right a bit...")
+		turn(+2, 0.5)
+		
+def drop_token():	   	
+
+def main():
+	while 1:
+	    pass
+	    
+main()	    
